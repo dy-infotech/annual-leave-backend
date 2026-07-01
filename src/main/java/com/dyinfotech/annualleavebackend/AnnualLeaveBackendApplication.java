f1833aa84447d7797956cd53732fcaddf558396d
leeseowoo <leeseowoo.kr@gmail.com>
2026-07-01 20:31:37 +0900

    chore: 프로젝트 셋업

---
+++ src/main/java/com/dyinfotech/annualleavebackend/AnnualLeaveBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dyinfotech.annualleavebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnualLeaveBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnnualLeaveBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dyinfotech/annualleavebackend/AnnualLeaveBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dyinfotech.annualleavebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnualLeaveBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
