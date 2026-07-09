7f18ca6d9e5f41d1490a2bb36d1724a61da9efaa
유영선 <dreamdragon7@naver.com>
2026-07-09 11:37:44 +0900

    style: remove unused import statement

--- src/main/java/com/dyinfotech/annualleavebackend/domain/LeaveAdjustment.java
+++ src/main/java/com/dyinfotech/annualleavebackend/domain/LeaveAdjustment.java
@@ -1,12 +1,9 @@
 package com.dyinfotech.annualleavebackend.domain;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
