c45701455fc304e2345c85aa396ee7dc0bca936d
유영선 <dreamdragon7@naver.com>
2026-07-09 13:38:05 +0900

    chore: init table

--- sql/data.sql
+++ sql/data.sql
@@ -1,4 +1,8 @@
-INSERT INTO employee (employee_number, name, department, position, hire_date, role, total_leave_days)
+INSERT INTO employee (employee_number, name, department, team, position, hire_date, curr_year, curr_total_leave_days)
 VALUES
-    ('A2025015', '이서우', 'SI사업팀', '사원', '2025-08-06', 'EMPLOYEE', 15.0),
-    ('A2020001', '이호영', 'SI사업팀', '이사', '2020-03-15', 'ADMIN', 15.0);
+    ('A2025015', '이서우', 'SI사업팀', '스마트팩토리구축사업', '사원', '2025-08-06', YEAR(SYSDATE()), 15.0),
+    ('A2020001', '이호영', 'SI사업팀', '스마트팩토리구축사업', '이사', '2020-03-15', YEAR(SYSDATE()), 15.0);
+
+INSERT INTO team (team, project_manager_id, parent_team)
+VALUES
+	('스마트팩토리구축사업', 2, '대표이사');
--- sql/schema.sql
+++ sql/schema.sql
@@ -2,50 +2,73 @@ CREATE DATABASE annual_leave CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE annual_leave;
 
 
-
-
-
-
 CREATE TABLE employee (
-                          employee_id       BIGINT AUTO_INCREMENT PRIMARY KEY,
-                          employee_number   VARCHAR(20)   NOT NULL UNIQUE COMMENT '사번',
-                          name              VARCHAR(50)   NOT NULL COMMENT '성명',
-                          department        VARCHAR(50)   NULL COMMENT '부서',
-                          position          VARCHAR(50)   NULL COMMENT '직급',
-                          hire_date         DATE          NOT NULL COMMENT '입사일',
+                          employee_id       	BIGINT			AUTO_INCREMENT PRIMARY KEY,
+                          employee_number   	VARCHAR(20) 	NOT NULL UNIQUE COMMENT '사번',
+                          password          	VARCHAR(255)	NULL COMMENT '비밀번호 (NULL일 경우 아직 회원가입 전)',
+                          name              	VARCHAR(50)   	NOT NULL COMMENT '성명',
+                          department        	VARCHAR(50)   	NULL COMMENT '부서',
+                          team		        	VARCHAR(30) 	NULL COMMENT '팀',
+                          position          	VARCHAR(50)		NULL COMMENT '직급',
+                          hire_date         	DATE        	NOT NULL COMMENT '입사일',
+                          fire_date         	DATE   			NULL COMMENT '퇴사일',
 
-                          login_id          VARCHAR(50)   NULL UNIQUE COMMENT '로그인 아이디 (NULL일 경우 아직 회원가입 전)',
-                          password          VARCHAR(255)  NULL,
-                          role              VARCHAR(10)   NOT NULL DEFAULT 'EMPLOYEE' COMMENT 'EMPLOYEE / ADMIN',
+                          curr_year				VARCHAR(4)  	NOT NULL COMMENT '올해 연도',
+                          curr_total_leave_days	FLOAT  			NOT NULL DEFAULT 0 COMMENT '올해 부여된 총 연차일수',
+                          prev_year				VARCHAR(4)  	NULL COMMENT '작년 연도',
+                          prev_total_leave_days	FLOAT  			NULL DEFAULT 0 COMMENT '작년 부여된 총 연차일수',
 
-                          total_leave_days  DECIMAL(4,1)  NOT NULL DEFAULT 0 COMMENT '올해 부여된 총 연차일수',
-
-                          created_at        DATETIME      NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                          updated_at        DATETIME      NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+                          created_at        	DATETIME      	NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                          approver_id          	BIGINT			NULL COMMENT '승인한 관리자 번호',
+                          updated_at        	DATETIME      	NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+                          
+                          CONSTRAINT fk_employee_team FOREIGN KEY (team) REFERENCES team(team)
 ) COMMENT '인사정보 + 로그인 계정 + 배정 연차';
 
-
-
-
-
-
 CREATE TABLE leave_request (
-                               leave_request_id   BIGINT AUTO_INCREMENT PRIMARY KEY,
-                               employee_id        BIGINT       NOT NULL COMMENT '신청자',
+                               leave_request_id   BIGINT		AUTO_INCREMENT PRIMARY KEY,
+                               employee_id        BIGINT		NOT NULL COMMENT '신청자',
 
-                               leave_type         VARCHAR(10)  NOT NULL COMMENT 'FULL(연차) / AM_HALF(오전반차) / PM_HALF(오후반차)',
-                               start_date         DATE         NOT NULL,
-                               end_date           DATE         NOT NULL,
-                               use_days           DECIMAL(4,1) NOT NULL COMMENT '차감될 일수 (연차 1.0 / 반차 0.5)',
+                               leave_type         VARCHAR(50)	NOT NULL COMMENT 'FULL(연차) / AM_HALF(오전반차) / PM_HALF(오후반차) / ALTERNATE(대체) / PARENTAL(출산) / FAMILY(가족돌봄) / OTHER(기타)',
+                               start_date         DATE			NOT NULL,
+                               end_date           DATE			NOT NULL,
+                               use_days           FLOAT			NOT NULL COMMENT '차감될 일수 (연차 1.0 / 반차 0.5)',
+                               leave_reason       VARCHAR(200)	NULL COMMENT '신청 사유',
 
-                               status             VARCHAR(10)  NOT NULL DEFAULT 'PENDING' COMMENT 'PENDING(대기) / APPROVED(승인 완료) / REJECTED(반려)',
-                               approver_id        BIGINT       NULL COMMENT '처리한 관리자',
-                               reject_reason      VARCHAR(200) NULL COMMENT '반려 사유',
-                               processed_at       DATETIME     NULL COMMENT '승인/반려 처리 시각',
+                               status             VARCHAR(10)	NOT NULL DEFAULT 'PENDING' COMMENT 'PENDING(대기) / APPROVED(승인 완료) / REJECTED(반려) / CANCELLED(신청자 취소)',
+                               manager_id         BIGINT		NULL COMMENT '처리한 관리자',
+                               managed_at         DATETIME		NULL COMMENT '처리 시각',
+                               reject_reason      VARCHAR(200)	NULL COMMENT '반려 사유',
 
-                               created_at         DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                               updated_at         DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                               created_at         DATETIME		NOT NULL DEFAULT CURRENT_TIMESTAMP
 
                                CONSTRAINT fk_request_employee FOREIGN KEY (employee_id) REFERENCES employee(employee_id),
-                               CONSTRAINT fk_request_approver FOREIGN KEY (approver_id) REFERENCES employee(employee_id)
-) COMMENT '연차/반차 신청 + 승인/반려 내역';
+                               CONSTRAINT fk_request_manager FOREIGN KEY (manager_id) REFERENCES employee(employee_id)
+) COMMENT '휴가 신청 + 승인/반려 내역';
+
+CREATE TABLE team (
+                               seq                BIGINT		AUTO_INCREMENT PRIMARY KEY,
+                               team               VARCHAR(30)	NOT NULL COMMENT '팀',
+                               project_manager_id BIGINT		NOT NULL COMMENT '프로젝트 담당자',
+                               parent_team        VARCHAR(30)	NOT NULL COMMENT '상위 팀'
+
+                               CONSTRAINT fk_project_manager FOREIGN KEY (project_manager_id) REFERENCES employee(employee_id)
+) COMMENT '팀 정보 (결재라인 상급자 탐색용)';
+
+CREATE TABLE basis_data (
+                               year               VARCHAR(4)	NOT NULL PRIMARY KEY,
+                               seq                BIGINT		NOT NULL PRIMARY KEY,
+                               type               VARCHAR(2)	NOT NULL COMMENT '0: bool, 1: int, 2: long, 3: float, 4: double, 5: string, ...',
+                               data               VARCHAR(50)	NOT NULL COMMENT '비고에 해당하는 값',
+                               remark             VARCHAR(200)	NOT NULL COMMENT '비고'
+) COMMENT '기초데이터 (연도별 연차 수, N년당 추가 연차 발생, 추가연차 발생시 연차 수, 만근 출석 퍼센트 등)';
+
+CREATE TABLE leave_adjustment (
+                               employee_id        BIGINT		NOT NULL PRIMARY KEY,
+                               year               VARCHAR(4)	NOT NULL PRIMARY KEY,
+                               sign               VARCHAR(5)	NOT NULL COMMENT 'plus, minus',
+                               leave_days         FLOAT			NOT NULL COMMENT '조정 일수',
+                               reason             VARCHAR(200)	NOT NULL COMMENT '발생 사유'
+                               CONSTRAINT fk_employee FOREIGN KEY (employee_id) REFERENCES employee(employee_id),
+) COMMENT '휴가 조정 내역(특별 휴가 추가, 만근 실패 차감)';
+
