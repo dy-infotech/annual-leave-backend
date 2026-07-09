0a89b6ee9c3b3c41322141506e630d531ff00893
유영선 <dreamdragon7@naver.com>
2026-07-09 12:41:51 +0900

    fix: @Entity requires @Id key, Float doesn't need precision and scale

--- src/main/java/com/dyinfotech/annualleavebackend/domain/Team.java
+++ src/main/java/com/dyinfotech/annualleavebackend/domain/Team.java
@@ -2,6 +2,9 @@ package com.dyinfotech.annualleavebackend.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -13,6 +16,11 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "seq")
+    private Long seq;
+	
     @Column(name = "team", length = 30)
     private String team;
 
