4cc4e8725ceeba89c9e5e8f0512d90d00f216a47
유영선 <dreamdragon7@naver.com>
2026-07-09 16:56:01 +0900

    feat: make employee_number prefix and insert table basis_data

--- sql/data.sql
+++ sql/data.sql
@@ -1,7 +1,17 @@
 INSERT INTO employee (employee_number, name, department, team, position, hire_date, curr_year, curr_total_leave_days)
 VALUES
     ('A2025015', '이서우', 'SI사업팀', '스마트팩토리구축사업', '사원', '2025-08-06', YEAR(SYSDATE()), 15.0),
-    ('A2020001', '이호영', 'SI사업팀', '스마트팩토리구축사업', '이사', '2020-03-15', YEAR(SYSDATE()), 15.0);
+    ('A2020001', '이호영', 'SI사업팀', '스마트팩토리구축사업', '이사', '2020-03-15', YEAR(SYSDATE()), 15.0)
+    ;
 
 INSERT INTO team (team, project_manager_id, parent_team)
 SELECT '스마트팩토리구축사업', employee_id, '대표이사' FROM employee WHERE name = '이호영';
+
+INSERT INTO basis_data (year, seq, type, data, remark)
+VALUES
+	(YEAR(SYSDATE()), 1, 1, '15', '1년차 연차일수'),
+	(YEAR(SYSDATE()), 2, 1, '2', 'N년당 추가연차 발생'),
+	(YEAR(SYSDATE()), 3, 1, '2', '추가연차 일수'),
+	(YEAR(SYSDATE()), 4, 3, '80.0', '만근 출석 퍼센트'),
+	(YEAR(SYSDATE()), 5, 5, 'A#{YEAR}', '사번 접두사')
+	;
