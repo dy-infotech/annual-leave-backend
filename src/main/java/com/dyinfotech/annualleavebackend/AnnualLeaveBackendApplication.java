68b6bdc5464a0a11be1aa22736bbf525fbf56cec
유영선 <dreamdragon7@naver.com>
2026-07-09 09:26:35 +0900

    feat: load .env file (for Windows)

--- src/main/java/com/dyinfotech/annualleavebackend/AnnualLeaveBackendApplication.java
+++ src/main/java/com/dyinfotech/annualleavebackend/AnnualLeaveBackendApplication.java
@@ -3,11 +3,17 @@ package com.dyinfotech.annualleavebackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class AnnualLeaveBackendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AnnualLeaveBackendApplication.class, args);
+    	// .env file load
+    	Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+    	dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+        
+    	SpringApplication.run(AnnualLeaveBackendApplication.class, args);
     }
 
 }
