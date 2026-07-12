0f45c3914607300aebe4843775d4c02707234872
유영선 <dreamdragon7@naver.com>
2026-07-12 21:17:43 +0900

    fix: @PostConstruct import error

--- src/main/java/com/dyinfotech/annualleavebackend/config/FirebaseConfig.java
+++ src/main/java/com/dyinfotech/annualleavebackend/config/FirebaseConfig.java
@@ -3,8 +3,6 @@ package com.dyinfotech.annualleavebackend.config;
 import java.io.IOException;
 import java.io.InputStream;
 
-import javax.annotation.PostConstruct;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
@@ -12,6 +10,8 @@ import org.springframework.core.io.Resource;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+
+import jakarta.annotation.PostConstruct;
 
 @Configuration
 public class FirebaseConfig {
