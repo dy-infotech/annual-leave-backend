7cb01f441f2ef5b71065aff639e893e6198d026b
유영선 <dreamdragon7@naver.com>
2026-07-13 10:15:06 +0900

    fix: table employee and Employee Object mapping

--- sql/schema.sql
+++ sql/schema.sql
@@ -12,7 +12,7 @@ CREATE TABLE employee (
                           department        	VARCHAR(50)   	NULL COMMENT '부서',
                           team		        	VARCHAR(30) 	NOT NULL COMMENT '팀 // 배정되지 않은 경우 대표이사 팀 선택 및 approver_id도 대표이사의 id로 해야 한다',
                           position          	VARCHAR(50)		NULL COMMENT '직급',
-                          email			 	    VARCHAR(50)		NULL COMMENT '이메일',
+                          email			 	    VARCHAR(100)		NULL COMMENT '이메일',
                           hire_date         	DATE        	NOT NULL COMMENT '입사일',
                           fire_date         	DATE   			NULL COMMENT '퇴사일',
 
