55186d5c4c7dcae8381618157300f74d8180ef03
유영선 <dreamdragon7@naver.com>
2026-07-14 09:29:12 +0900

    fix: override configure for external tomcat

--- src/main/java/com/dyinfotech/annualleavebackend/AnnualLeaveBackendApplication.java
+++ src/main/java/com/dyinfotech/annualleavebackend/AnnualLeaveBackendApplication.java
@@ -2,12 +2,19 @@ package com.dyinfotech.annualleavebackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
-public class AnnualLeaveBackendApplication {
+public class AnnualLeaveBackendApplication extends SpringBootServletInitializer {
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AnnualLeaveBackendApplication.class);
+    }
+	
     public static void main(String[] args) {
     	java.security.Security.setProperty("jdk.tls.disabledAlgorithms", "");
     	SpringApplication.run(AnnualLeaveBackendApplication.class, args);
