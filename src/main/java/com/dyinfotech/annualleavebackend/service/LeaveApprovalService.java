0c13cb8ac76ef080bcb6460c9a6e0762ab11e86e
유영선 <dreamdragon7@naver.com>
2026-07-14 10:04:07 +0900

    fix: approver check rollback

--- src/main/java/com/dyinfotech/annualleavebackend/service/LeaveApprovalService.java
+++ src/main/java/com/dyinfotech/annualleavebackend/service/LeaveApprovalService.java
@@ -76,11 +76,17 @@ public class LeaveApprovalService {
         
         // 관리자가 요청자의 승인자인지 확인 (로그인 시 업데이트되므로 문제되지 않으나 방어코드로 유지한다)
         Map.Entry<Boolean, Set<Long>> approverEntry = teamService.getEntryOfHasApproverAndApproverIds(employee);
-        if (!approverEntry.getKey()) {
-        	StringBuilder approverString = new StringBuilder();
-        	for (Long id : approverEntry.getValue()) {
-        		approverString.append(id).append(",");
-        	}
+        boolean isApprover = false;
+        Set<Long> expectedApproverIds = approverEntry.getValue();
+    	StringBuilder approverString = new StringBuilder();
+    	for (Long id : approverEntry.getValue()) {
+    		if (id.equals(approverId)) {
+    			isApprover = true;
+    			break;
+    		}
+    		approverString.append(id).append(",");
+    	}
+        if (!isApprover) {
         	if (!approverString.isEmpty()) {
         		approverString.setLength(approverString.length() - 1);
         	}
