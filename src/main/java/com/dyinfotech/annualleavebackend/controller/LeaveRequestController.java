28fa21dfd1562ca2f5b2f07bb0e4605988f7a66d
유영선 <dreamdragon7@naver.com>
2026-07-14 14:30:46 +0900

    fix: LeaveRequest API parameter parsing error

--- src/main/java/com/dyinfotech/annualleavebackend/controller/LeaveRequestController.java
+++ src/main/java/com/dyinfotech/annualleavebackend/controller/LeaveRequestController.java
@@ -1,23 +1,20 @@
 package com.dyinfotech.annualleavebackend.controller;
 
-import java.time.LocalDate;
 import java.util.List;
 
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.dyinfotech.annualleavebackend.common.type.LeaveRequestStatus;
 import com.dyinfotech.annualleavebackend.dto.LeaveRequestDto;
 import com.dyinfotech.annualleavebackend.dto.LeaveRequestListDto;
 import com.dyinfotech.annualleavebackend.service.LeaveRequestService;
@@ -40,12 +37,8 @@ public class LeaveRequestController {
 
     @GetMapping("/all")
     public List<LeaveRequestListDto.LeaveRequestListResponse> searchLeaveRequests(
-            @RequestParam(required = false) Long employeeId,
-            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
-            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate,
-            @RequestParam(required = false) LeaveRequestStatus status
+    		@ModelAttribute LeaveRequestListDto.LeaveRequestListRequest condition
     ) {
-        LeaveRequestListDto.LeaveRequestListRequest condition = new LeaveRequestListDto.LeaveRequestListRequest(employeeId, startDate, endDate, status);
         return leaveRequestService.searchLeaveRequests(condition);
     }
 
@@ -53,11 +46,9 @@ public class LeaveRequestController {
     @GetMapping("/my")
     public List<LeaveRequestListDto.LeaveRequestListResponse> searchMyLeaveRequests(
             @AuthenticationPrincipal Long employeeId,
-            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
-            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate,
-            @RequestParam(required = false) LeaveRequestStatus status
+    		@ModelAttribute LeaveRequestListDto.LeaveRequestListRequest condition
     ) {
-        LeaveRequestListDto.LeaveRequestListRequest condition = new LeaveRequestListDto.LeaveRequestListRequest(employeeId, startDate, endDate, status);
+    	condition.setEmployeeId(employeeId);
         return leaveRequestService.searchLeaveRequests(condition);
     }
 
