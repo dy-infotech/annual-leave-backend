16d3a0f8e8d7f0c2bd90f49c3e14f991f47e06a3
유영선 <dreamdragon7@naver.com>
2026-07-15 13:49:44 +0900

    style: remove unused import

--- src/main/java/com/dyinfotech/annualleavebackend/service/LeaveApprovalService.java
+++ src/main/java/com/dyinfotech/annualleavebackend/service/LeaveApprovalService.java
@@ -7,7 +7,6 @@ import java.util.Map;
 import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Caching;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
--- src/main/java/com/dyinfotech/annualleavebackend/service/LeaveRequestService.java
+++ src/main/java/com/dyinfotech/annualleavebackend/service/LeaveRequestService.java
@@ -7,7 +7,6 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.Caching;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
