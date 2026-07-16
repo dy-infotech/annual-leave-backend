80121adb96f11421ed56491bc2e5a1308829e419
유영선 <dreamdragon7@naver.com>
2026-07-16 11:55:41 +0900

    feat: ROOT.war size lower

--- src/main/java/com/dyinfotech/annualleavebackend/config/CommonConfig.java
+++ src/main/java/com/dyinfotech/annualleavebackend/config/CommonConfig.java
@@ -5,14 +5,10 @@ import java.time.Duration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.http.client.reactive.JdkClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-import io.netty.channel.ChannelOption;
-import reactor.netty.http.client.HttpClient;
 
 @Configuration
 public class CommonConfig {
@@ -31,13 +27,23 @@ public class CommonConfig {
     @Bean
     @Primary
     WebClient webClient(WebClient.Builder builder) {
-        // Reactor Netty의 HttpClient 설정
-        HttpClient httpClient = HttpClient.create()
-                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 10000)	// 연결 타임아웃 10초
-                .responseTimeout(Duration.ofSeconds(10));				// 응답 타임아웃 10초
-
-        return builder
-                .clientConnector(new ReactorClientHttpConnector(httpClient))
+//        // Reactor Netty의 HttpClient 설정
+//        reactor.netty.http.client.HttpClient httpClient = reactor.netty.http.client.HttpClient
+//        																			.create()
+//																	                .option(io.netty.channel.ChannelOption.CONNECT_TIMEOUT_MILLIS, 10000)	// 연결 타임아웃 10초
+//																	                .responseTimeout(Duration.ofSeconds(10));				// 응답 타임아웃 10초
+//
+//        return builder
+//                .clientConnector(new ReactorClientHttpConnector(httpClient))
+//                .build();
+    	// Java 11+ 내장 HttpClient 생성 및 설정 (타임아웃 등)
+    	java.net.http.HttpClient httpClient = java.net.http.HttpClient.newBuilder()
+														                .connectTimeout(Duration.ofSeconds(10))
+														                .build();
+        
+        // Netty 대신 JdkClientHttpConnector를 사용하도록 WebClient 빌드
+        return WebClient.builder()
+                .clientConnector(new JdkClientHttpConnector(httpClient))
                 .build();
     }
 }
