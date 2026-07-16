adbfc19a68af4811f0bee3f8a45383c3db3f2a9e
박묘성 <aytjd333@naver.com>
2026-07-16 09:27:40 +0900

    feat: employee use email

--- sql/data.sql
+++ sql/data.sql
@@ -1,9 +1,9 @@
-INSERT INTO employee (employee_number, name, department, team, position, hire_date, curr_year, curr_total_leave_days)
+INSERT INTO employee (employee_number, name, department, team, email, position, hire_date, curr_year, curr_total_leave_days)
 VALUES
-    ('A2025016', '최민지', 'SI사업팀', '스마트팩토리구축사업', '사원', '2025-08-04', YEAR(SYSDATE()), 15.0),
-    ('A2025015', '이서우', 'SI사업팀', '스마트팩토리구축사업', '사원', '2025-08-06', YEAR(SYSDATE()), 15.0),
-    ('A2020001', '이호영', 'SI사업팀', '스마트팩토리구축사업', '이사', '2020-03-15', YEAR(SYSDATE()), 15.0),
-    ('A2011001', '우동영', 'SI사업팀', '스마트팩토리구축사업', '대표이사', '2011-01-01', YEAR(SYSDATE()), 22.0)
+    ('A2025016', '최민지', 'SI사업팀', '스마트팩토리구축사업', 'test@test.com', '사원', '2025-08-04', YEAR(SYSDATE()), 15.0),
+    ('A2025015', '이서우', 'SI사업팀', '스마트팩토리구축사업', 'test@test.com', '사원', '2025-08-06', YEAR(SYSDATE()), 15.0),
+    ('A2020001', '이호영', 'SI사업팀', '스마트팩토리구축사업', 'test@test.com', '이사', '2020-03-15', YEAR(SYSDATE()), 15.0),
+    ('A2011001', '우동영', 'SI사업팀', '스마트팩토리구축사업', 'test@test.com', '대표이사', '2011-01-01', YEAR(SYSDATE()), 22.0)
     ;
 
 INSERT INTO team (team, project_manager_id, parent_team)
