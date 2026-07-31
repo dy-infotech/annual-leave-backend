33eea4ddd98e2ce52cc1cca0d3e8364acbf14462
유영선 <dreamdragon7@naver.com>
2026-07-31 15:58:16 +0900

    refactor: remove unused import

--- src/main/java/com/dyinfotech/annualleavebackend/dto/FindDataDto.java
+++ src/main/java/com/dyinfotech/annualleavebackend/dto/FindDataDto.java
@@ -2,7 +2,6 @@ package com.dyinfotech.annualleavebackend.dto;
 
 import java.util.Collection;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
