ded5fcaf743ac816dcbe1497e023a2c1cdfdade2
유영선 <dreamdragon7@naver.com>
2026-08-03 16:37:31 +0900

    fix: IpFilter enable

--- src/main/java/com/dyinfotech/annualleavebackend/filter/IpFilter.java
+++ src/main/java/com/dyinfotech/annualleavebackend/filter/IpFilter.java
@@ -14,7 +14,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-//@Component
+@Component
 public class IpFilter extends OncePerRequestFilter {
     private static final String MDC_KEY = "clientIp";
     
