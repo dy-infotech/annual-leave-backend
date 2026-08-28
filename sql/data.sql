5a0e73f9a73f2aada0bf6ac3951cd305574465ba
minji <bellatempo@naver.com>
2026-08-28 13:29:13 +0900

    prev_total_leave_days curr_total_leave_days 추가

--- sql/data.sql
+++ sql/data.sql
@@ -51,7 +51,7 @@ VALUES
 	(YEAR(SYSDATE()), 7, 5, 'https://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService', '한국천문연구원_특일 정보 API 서비스 URL'),
 	(YEAR(SYSDATE()), 8, 5, 'getRestDeInfo', '한국천문연구원_특일 정보 API 공휴일 요청 주소'),
 	(YEAR(SYSDATE()), 9, 1, '30', '로그인 실패 최대 횟수'),
-	(YEAR(SYSDATE()), 10, 1, '24', '로그인 실패 잠금 해제까지 남은 시각')
+	(YEAR(SYSDATE()), 10, 1, '24', '로그인 실패 잠금 해제까지 남은 시각'),
 	(YEAR(SYSDATE()), 11, 0, 'false', '회계연도 정책 사용 여부')
 	;
 	
--- sql/schema.sql
+++ sql/schema.sql
@@ -41,6 +41,8 @@ CREATE TABLE leave_request (
                                start_date         DATE			NOT NULL,
                                end_date           DATE			NOT NULL,
                                use_days           FLOAT			NOT NULL COMMENT '차감될 일수 (연차 1.0 / 반차 0.5)',
+                               prev_total_leave_days FLOAT		NOT NULL COMMENT '휴가 신청 전의 연차 일수',
+                               curr_total_leave_days FLOAT		NOT NULL COMMENT '휴가 신청 후의 연차 일수',
                                leave_reason       VARCHAR(200)	NULL COMMENT '신청 사유',
 
                                status             VARCHAR(10)	NOT NULL DEFAULT 'PENDING' COMMENT 'PENDING(대기) / APPROVED(승인 완료) / REJECTED(반려) / CANCELLED(신청자 취소)',
@@ -55,6 +57,7 @@ CREATE TABLE leave_request (
                                CONSTRAINT fk_request_employee FOREIGN KEY (employee_id) REFERENCES employee(employee_id),
                                CONSTRAINT fk_request_manager FOREIGN KEY (manager_id) REFERENCES employee(employee_id)
 ) COMMENT '휴가 신청 + 승인/반려 내역';
+
 
 
 
