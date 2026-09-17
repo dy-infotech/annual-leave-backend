1b1718b15b7a212cc4b84a938a76aebdd07a383b
solebloom <48436057+solebloom@users.noreply.github.com>
2026-09-17 14:10:49 +0900

    fix: handle leap-day leave anniversary

--- src/main/java/com/dyinfotech/annualleavebackend/common/util/DateUtils.java
+++ src/main/java/com/dyinfotech/annualleavebackend/common/util/DateUtils.java
@@ -16,6 +16,26 @@ public class DateUtils {
 		return year.atMonth(Month.DECEMBER).atEndOfMonth();
 	}
 	
+	public static final LocalDate getAnniversaryDate(LocalDate hireDate, int year) {
+		if (hireDate.getMonthValue() == 2 && hireDate.getDayOfMonth() == 29 && !Year.isLeap(year)) {
+			return LocalDate.of(year, 3, 1);
+		}
+		return hireDate.withYear(year);
+	}
+	
+	public static final LocalDate getLeaveYearStartDate(LocalDate hireDate, LocalDate targetDate) {
+		LocalDate anniversary = getAnniversaryDate(hireDate, targetDate.getYear());
+		if (anniversary.isAfter(targetDate)) {
+			anniversary = getAnniversaryDate(hireDate, targetDate.getYear() - 1);
+		}
+		return anniversary;
+	}
+	
+	public static final LocalDate getLeaveYearEndDate(LocalDate hireDate, LocalDate targetDate) {
+		LocalDate startDate = getLeaveYearStartDate(hireDate, targetDate);
+		return getAnniversaryDate(hireDate, startDate.getYear() + 1).minusDays(1);
+	}
+	
 	public static final int toMinutes(@NotNull Float useDays) {
 		return Math.round(useDays * CommonConfig.DAILY_STANDARD_WORKING_MINUTES);
 	}
