8e8556fc3851d3a49e8251ce90218a735cada8c2
solebloom <48436057+solebloom@users.noreply.github.com>
2026-09-18 16:29:23 +0900

    fix: initialize team project manager for cache

--- src/main/java/com/dyinfotech/annualleavebackend/repository/TeamRepository.java
+++ src/main/java/com/dyinfotech/annualleavebackend/repository/TeamRepository.java
@@ -2,12 +2,17 @@ package com.dyinfotech.annualleavebackend.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.dyinfotech.annualleavebackend.domain.Team;
 
 public interface TeamRepository extends JpaRepository<Team, Long> {
+	@Override
+	@EntityGraph(attributePaths = "projectManager")
+	List<Team> findAll();
 	boolean existsByProjectManager_EmployeeId(Long projectManagerId);
 	Team findFirstByTeamOrderBySeqAsc(String team);
+    @EntityGraph(attributePaths = "projectManager")
     List<Team> findAllByTeamOrderBySeqAsc(String team);
 }
